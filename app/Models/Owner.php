ca95ec8157145713d9445441dd24f0dc635b3af5
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-11-15 13:45:07 +0900

    Userモデルを参考にOwnerモデルに$fillable, $hidden, $castsを追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -9,4 +9,35 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class Owner extends Authenticatable
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
