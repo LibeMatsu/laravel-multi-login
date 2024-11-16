26883fe69336cdb279b22de21d8fc67c9bc0bd43
matsuki-kayoko <matsuki.1067@universal-osaka.com>
2024-11-16 16:28:01 +0900

    adminとownerのwelcome.bledeに抜けがあったので追記。Route::hasは指定した名前のルートが定義されているかチェックするメソッド

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -24,7 +24,7 @@
                     @else
                         <a href="{{ route('admin.login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
+                        @if (Route::has('admin.register'))
                             <a href="{{ route('admin.register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
                         @endif
                     @endauth
